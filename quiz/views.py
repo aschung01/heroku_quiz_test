e8be8f5fd17ed50eeb0c663cda568cd840850961
aschung01 <achung20@gmail.com>
2021-09-17 22:05:39 +0900

    Resolved error

--- quiz/views.py
+++ quiz/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Quiz
 from .serializers import QuizSerializer
+import random
 
 # Create your views here.
 @api_view(['GET'])
